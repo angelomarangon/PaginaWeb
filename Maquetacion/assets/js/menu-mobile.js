28fd078a7f6767ef6ff8b39d697629d0f9a3f137
angelomarangon <angelo.marangon1@icloud.com>
2024-05-05 15:25:59 +0200

    actualizacion menu responsive

--- Maquetacion/assets/js/menu-mobile.js
+++ Maquetacion/assets/js/menu-mobile.js
@@ -13,7 +13,27 @@ document.addEventListener("DOMContentLoaded", e => {
     })
 
 
-    let optionService = document.querySelectorAll('.menu-mobile__option')
+    let option = document.querySelectorAll('.menu-mobile__option')
+    let icon = document.querySelectorAll('.menu-mobile__icon');
+    let submenu = document.querySelectorAll('.menu-mobile__submenu');
+
+    option[1].addEventListener('click', ()=>{
+        icon[0].classList.toggle('menu-mobile__icon--active');
+
+        submenu[0].classList.toggle('menu-mobile__submenu--active');
+
+    })
+    option[2].addEventListener('click', ()=>{
+        icon[1].classList.toggle('menu-mobile__icon--active');
+
+        submenu[1].classList.toggle('menu-mobile__submenu--active');
+    })
+    option[3].addEventListener('click', ()=>{
+        icon[2].classList.toggle('menu-mobile__icon--active');
+
+        submenu[2].classList.toggle('menu-mobile__submenu--active');
+    })
+    
 
 
 })
